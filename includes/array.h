12f684486b792e5d47b7b7309d5deb670ac15626
Vincent S. <vs38190@gmail.com>
2025-02-16 17:45:10 +0100

    Add thread safety to dynamic array operations

--- includes/array.h
+++ includes/array.h
@@ -1,5 +1,20 @@
 #include <assert.h>
 
+#ifdef _DA_THREAD_SAFE
+#include <pthread.h>
+#define _DA_MUTEX pthread_mutex_t lock;
+#define _da_lock(da) pthread_mutex_lock(&(da)->lock)
+#define _da_unlock(da) pthread_mutex_unlock(&(da)->lock)
+#define _da_init(da) pthread_mutex_init(&(da)->lock, NULL)
+#define _da_destroy(da) pthread_mutex_destroy(&(da)->lock)
+#else
+#define _DA_MUTEX
+#define _da_lock(da)
+#define _da_unlock(da)
+#define _da_init(da)
+#define _da_destroy(da)
+#endif
+
 // Reallocate memory for the dynamic array (must have the same signature as
 // realloc)
 #ifndef _DA_REALLOC
@@ -48,8 +63,8 @@
     }                                                                          \
   } while (0)
 
-// Append an item to the dynamic array
-#define da_append(da, item)                                                    \
+// Append an item to the dynamic array without locking
+#define da_append_unsafe(da, item)                                             \
   do {                                                                         \
     if ((da)->count >= (da)->capacity) {                                       \
       _da_increase(da);                                                        \
@@ -58,17 +73,33 @@
     (da)->items[(da)->count++] = item;                                         \
   } while (0)
 
-// Resize the dynamic array to the new count
-#define da_resize(da, count)                                                   \
+// Append an item to the dynamic array
+#define da_append(da, item)                                                    \
   do {                                                                         \
-    if ((da)->capacity < (count)) {                                            \
-      (da)->capacity = (count);                                                \
+    _da_lock(da);                                                              \
+    da_append_unsafe(da, item);                                                \
+    _da_unlock(da);                                                            \
+  } while (0)
+
+// Resize the dynamic array to the new target without locking
+#define da_resize_unsafe(da, _capacity)                                        \
+  do {                                                                         \
+    if ((da)->capacity < (_capacity)) {                                        \
+      (da)->capacity = (_capacity);                                            \
       _da_realloc(da);                                                         \
     }                                                                          \
   } while (0)
 
-// Append multiple items to the dynamic array
-#define da_append_many(da, _items, _count)                                     \
+// Resize the dynamic array to the new target
+#define da_resize(da, _capacity)                                               \
+  do {                                                                         \
+    _da_lock(da);                                                              \
+    da_resize_unsafe(da, _capacity);                                           \
+    _da_unlock(da);                                                            \
+  } while (0)
+
+// Append multiple items to the dynamic array without locking
+#define da_append_many_unsafe(da, _items, _count)                              \
   do {                                                                         \
     if ((da)->count + (_count) > (da)->capacity) {                             \
       while ((da)->count + (_count) > (da)->capacity) {                        \
@@ -81,8 +112,16 @@
     (da)->count += (_count);                                                   \
   } while (0)
 
-// Free the dynamic array
-#define da_free(da)                                                            \
+// Append multiple items to the dynamic array
+#define da_append_many(da, _items, _count)                                     \
+  do {                                                                         \
+    _da_lock(da);                                                              \
+    da_append_many_unsafe(da, _items, _count);                                 \
+    _da_unlock(da);                                                            \
+  } while (0)
+
+// Free the dynamic array without locking
+#define da_free_unsafe(da)                                                     \
   do {                                                                         \
     if ((da)->items != NULL)                                                   \
       free((da)->items);                                                       \
@@ -91,34 +130,85 @@
     (da)->capacity = 0;                                                        \
   } while (0)
 
+// Free the dynamic array
+#define da_free(da)                                                            \
+  do {                                                                         \
+    _da_lock(da);                                                              \
+    da_free_unsafe(da);                                                        \
+    _da_unlock(da);                                                            \
+    _da_destroy(da);                                                           \
+  } while (0)
+
+// Clear the dynamic array without locking
+#define da_clear_unsafe(da)                                                    \
+  do {                                                                         \
+    (da)->count = 0;                                                           \
+  } while (0)
+
 // Clear the dynamic array
 #define da_clear(da)                                                           \
   do {                                                                         \
-    (da)->count = 0;                                                           \
+    _da_lock(da);                                                              \
+    da_clear_unsafe(da);                                                       \
+    _da_unlock(da);                                                            \
+  } while (0)
+
+// Remove an item from the dynamic array without locking (**does not preserve
+// order**)
+#define da_fast_remove_unsafe(da, index)                                       \
+  do {                                                                         \
+    (da)->items[(index)] = (da)->items[((da)->count--) - 1];                   \
   } while (0)
 
 // Remove an item from the dynamic array (**does not preserve order**)
 #define da_fast_remove(da, index)                                              \
   do {                                                                         \
-    (da)->items[(index)] = (da)->items[((da)->count--) - 1];                   \
+    _da_lock(da);                                                              \
+    da_fast_remove_unsafe(da, index);                                          \
+    _da_unlock(da);                                                            \
   } while (0)
 
-// Remove an item from the dynamic array
-#define da_remove(da, index)                                                   \
+// Remove an item from the dynamic array without locking
+#define da_remove_unsafe(da, index)                                            \
   do {                                                                         \
     _DA_MEMCPY((da)->items + index, (da)->items + index + 1,                   \
                (--(da)->count) - index);                                       \
   } while (0)
 
-// Shrink the dynamic array to the count item
-#define da_shrink(da)                                                          \
+// Remove an item from the dynamic array
+#define da_remove(da, index)                                                   \
+  do {                                                                         \
+    _da_lock(da);                                                              \
+    da_remove_unsafe(da, index);                                               \
+    _da_unlock(da);                                                            \
+  } while (0)
+
+// Shrink the dynamic array to the count item without locking
+#define da_shrink_unsafe(da)                                                   \
   do {                                                                         \
     (da)->capacity = (da)->count;                                              \
     _da_realloc(da);                                                           \
+  } while (0)
+
+// Shrink the dynamic array to the count item
+#define da_shrink(da)                                                          \
+  do {                                                                         \
+    _da_lock(da);                                                              \
+    da_shrink_unsafe(da);                                                      \
+    _da_unlock(da);                                                            \
+  } while (0)
+
+#define da_init(da)                                                            \
+  do {                                                                         \
+    (da)->count = 0;                                                           \
+    (da)->capacity = 0;                                                        \
+    (da)->items = NULL;                                                        \
+    _da_init(da);                                                              \
   } while (0)
 
 // Define the dynamic array structure elements for a given type
 #define da_struct(type)                                                        \
   size_t count;                                                                \
   size_t capacity;                                                             \
+  _DA_MUTEX                                                                    \
   type *items;
